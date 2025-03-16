c5cdf885b67e2ed5f40dacb0d87c01e1ff1e4afe
Luis Muñoz <lumusa2design@gmail.com>
2025-03-16 02:05:27 +0000

    added main method to search algorithms and modified bisection method to reduce redundancy

--- search algorithms/bisection_method.py
+++ search algorithms/bisection_method.py
@@ -1,9 +1,8 @@
 def bisection_method(func, a, b, tol=1e-9):
-    if func(a) < func(b):
+    if func(a) < 0:
         minim,maxim = a,b
     else:
         minim,maxim = b,a
-    flag = True
     while(True):
         c = (minim + maxim)/2
         if func(c) < 0:
@@ -12,7 +11,4 @@ def bisection_method(func, a, b, tol=1e-9):
             maxim = c
         if(func(c) == 0):
             return c
-
-def func(x):
-    return 2*x - 2
 
---
+++ search algorithms/search_algoritms_main.py
@@ -0,0 +1,29 @@
+from bisection_method import bisection_method
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x):
+    return x**3 - 4*x + 1
+
+def view_graphic(f, inicio, fin, method,a,b):
+    x = np.linspace(inicio,fin,(fin+abs(inicio)*100))
+    y = f(x)
+
+    plt.figure(figsize=(8,5))
+    plt.plot(x,y, label="funcion", color="b")
+    plt.axhline(0, color="black", linewidth=1)  # Eje x
+    plt.axvline(0, color="black", linewidth=1)  # Eje y
+    plt.grid(True, linestyle="--", alpha=0.6)
+
+    punto_x = method(f,a,b)
+    punto_y = f(punto_x)
+
+    plt.scatter(punto_x, punto_y, color="red", zorder=3, label=f"Punto ({punto_x}, {punto_y:.2f})")
+    plt.title("Gráfica de la función")
+    plt.xlabel("x")
+    plt.ylabel("f(x)")
+    plt.legend()
+
+    plt.show()
+view_graphic(f, -4, 4, bisection_method, 0, 2)
+
