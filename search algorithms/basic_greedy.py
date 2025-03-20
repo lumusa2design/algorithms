1a129704ec18da4ec07759270168579b4ddaab36
Luis Muñoz <lumusa2design@gmail.com>
2025-03-20 02:13:27 +0000

    Added a basic greedy algorithm

---
+++ search algorithms/basic_greedy.py
@@ -0,0 +1,13 @@
+def greedy(num):
+    coins = [50,20,10,5,2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01]
+    res = []
+    i = 0
+    while num >= 1e-2:
+        if coins[i] >= num:
+            i+=1
+        else:
+            res.append(coins[i])
+            num -= coins[i]
+    return res
+
+print(greedy(12.58))
