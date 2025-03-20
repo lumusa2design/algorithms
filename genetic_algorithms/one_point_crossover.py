4b61867208de60ec34932eac89effe91388e7889
Luis Muñoz <lumusa2design@gmail.com>
2025-03-20 02:32:23 +0000

    fixed little problems

--- genetic_algorithms/one_point_crossover.py
+++ genetic_algorithms/one_point_crossover.py
@@ -1,3 +1,3 @@
 def one_point_crossover(point, father, mother):
-    father[:point], mother[:point] = mother[:3], father[:point]
+    father[:point], mother[:point] = mother[:point], father[:point]
     return father, mother
