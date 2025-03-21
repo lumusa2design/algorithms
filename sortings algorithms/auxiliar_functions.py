10134f82cdfde1400bcfbe49908fa2cab1cc4695
Luis Muñoz <lumusa2design@gmail.com>
2025-03-21 02:27:47 +0000

    Added counting sort and max function to auxiliar functions

--- sortings algorithms/auxiliar_functions.py
+++ sortings algorithms/auxiliar_functions.py
@@ -4,3 +4,10 @@ def is_order(lista):
         if lista[i] > lista[i+1]:
             flag = False
     return flag
+
+def find_max(arr):
+    maxim = arr[0]
+    for i in range(1,len(arr)):
+        if arr[i] > maxim:
+            maxim = arr[i]
+    return maxim
---
+++ sortings algorithms/counting_sort.py
@@ -0,0 +1,18 @@
+from auxiliar_functions import find_max
+
+def counting_sort(arr):
+    maxim = find_max(arr)
+    count = [0] * (maxim +1)
+    res = []
+    for i in range(len(arr)):
+        count[arr[i]] += 1
+
+    for i in range(len(count)):
+        while count[i] > 0:
+            res.append(i)
+            count[i] -= 1
+    return res
+
+
+
+
