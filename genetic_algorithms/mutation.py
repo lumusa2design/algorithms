b07cceeb477db361bbecfdd29a9065fc62f956dc
Luis Muñoz <lumusa2design@gmail.com>
2025-03-25 21:59:31 +0000

    tested mutation algorithm

--- genetic_algorithms/mutation.py
+++ genetic_algorithms/mutation.py
@@ -1,8 +1,12 @@
 import random
 
 def mutation(arr, space):
-    for i in range(arr):
+    for i in range(len(arr)):
         if random.choice([True, False]):
             arr[i] = random.choice(space)
     return arr
 
+arr = [1,2,3,4,5,6,7,8,9,10]
+space = [1,2,3,4,5,6,7,8,9,10]
+print(arr)
+print(f'{mutation(arr, space)} es el nuevo hijo')
