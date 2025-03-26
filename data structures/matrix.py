121d4f6f4bd194baeeb822ca52d1a4d51b00e0a2
Luis Muñoz <lumusa2design@gmail.com>
2025-03-26 17:36:26 +0000

    Added basic matrix to futures algorithms

---
+++ data structures/matrix.py
@@ -0,0 +1,16 @@
+def create_matrix(col, row):
+    arr = []
+    for i in range(col):
+        arr.append([])
+        for j in range(row):
+            arr[i].append(0)
+    print(arr)
+    fill_matrix(arr)
+    return arr
+def fill_matrix(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            arr[i][j] = input(f'introduzca el valor para la posicion{i},{j}: ')
+
+arr = create_matrix(2,1)
+print(arr)
