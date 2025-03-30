b89671b09f9f71a9aea1e13d8961cf593a2d721a
Luis Muñoz <lumusa2design@gmail.com>
2025-03-30 00:42:55 +0000

    added quicksort test

--- sortings algorithms/sort_test.py
+++ sortings algorithms/sort_test.py
@@ -1,11 +1,16 @@
 import unittest
 from auxiliar_functions import is_order
 from merge_sort import merge_sort
-
+from quick_sort import quicksort
 class TestSortMethods(unittest.TestCase):
     def test_merge(self):
         arr = [10,9,8,7,6,42,1]
         arr = merge_sort(arr)
+        self.assertTrue(is_order(arr))
+
+    def test_quicksort(self):
+        arr = [10, 9, 8, 7, 6, 42, 1]
+        arr = quicksort(arr)
         self.assertTrue(is_order(arr))
 
 if __name__=='__main__':
