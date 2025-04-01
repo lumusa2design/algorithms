d0e635557dd30432c163abbcd8ef212cbb63c002
Luis Muñoz <lumusa2design@gmail.com>
2025-04-01 17:01:37 +0100

    Added test of bubblesort

--- sortings algorithms/sort_test.py
+++ sortings algorithms/sort_test.py
@@ -3,6 +3,7 @@ from auxiliar_functions import is_order
 from merge_sort import merge_sort
 from quick_sort import quicksort
 from bogo_sort import bogo_sort
+from bubble_sort import bubble_sort
 class TestSortMethods(unittest.TestCase):
     def test_merge(self):
         arr = [10,9,8,7,6,42,1]
@@ -18,5 +19,10 @@ class TestSortMethods(unittest.TestCase):
         arr = [10, 9, 8, 7, 6, 42, 1]
         arr = bogo_sort(arr)
         self.assertTrue(is_order(arr))
+    def test_bubblesort(self):
+        arr = [10, 9, 8, 7, 6, 42, 1]
+        arr = bubble_sort(arr)
+        self.assertTrue(is_order(arr))
+
 if __name__=='__main__':
     unittest.main()
