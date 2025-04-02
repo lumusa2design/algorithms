5133a557ed8824d11b48b34421925a2e16122ad5
Luis Muñoz <lumusa2design@gmail.com>
2025-04-02 12:49:55 +0100

    Added counting sort test

--- sortings algorithms/sort_test.py
+++ sortings algorithms/sort_test.py
@@ -4,6 +4,7 @@ from merge_sort import merge_sort
 from quick_sort import quicksort
 from bogo_sort import bogo_sort
 from bubble_sort import bubble_sort
+from counting_sort import counting_sort
 class TestSortMethods(unittest.TestCase):
     def test_merge(self):
         arr = [10,9,8,7,6,42,1]
@@ -23,7 +24,10 @@ class TestSortMethods(unittest.TestCase):
         arr = [10, 9, 8, 7, 6, 42, 1]
         arr = bubble_sort(arr)
         self.assertTrue(is_order(arr))
-
+    def test_countingsort(self):
+        arr = [10, 9, 8, 7, 6, 42, 1]
+        arr = counting_sort(arr)
+        self.assertTrue(is_order(arr))
 
 if __name__=='__main__':
     unittest.main()
