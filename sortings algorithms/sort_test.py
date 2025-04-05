b3c6426830a40f2e71b93362b0595c0132d183c1
Luis Muñoz <lumusa2design@gmail.com>
2025-04-05 19:20:26 +0100

    Added test

--- sortings algorithms/sort_test.py
+++ sortings algorithms/sort_test.py
@@ -7,6 +7,7 @@ from bubble_sort import bubble_sort
 from counting_sort import counting_sort
 from stalin_sort import stalin_sort
 from selection_sort import selection_sort
+
 class TestSortMethods(unittest.TestCase):
 
 
@@ -32,6 +33,11 @@ class TestSortMethods(unittest.TestCase):
     def test_countingsort(self):
         arr = [10, 9, 8, 7, 6, 42, 1]
         arr = counting_sort(arr)
+        self.assertTrue(is_order(arr))
+
+    def test_insertionsort(self):
+        arr = [10, 9, 8, 7, 6, 42, 1]
+        arr = inser(arr)
         self.assertTrue(is_order(arr))
 
     def test_staling_sort(self):
