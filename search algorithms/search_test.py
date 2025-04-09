47fc5de74c8be39ef803c2674ebbbcf9cec7a45f
Luis Muñoz <lumusa2design@gmail.com>
2025-04-09 19:07:02 +0100

    fixed and improve test

--- search algorithms/search_test.py
+++ search algorithms/search_test.py
@@ -1,11 +1,10 @@
 import unittest
 from lineal_search import linear_search
 class SearchTestMethods(unittest.TestCase):
-    def linear_Search_test(self):
+    def test_lineal_search(self):
         arr = [1,2,3,4,5,6,7]
         search = 5
-        result  =  linear_search(arr, 5)
-        print(result)
-        self.assertTrue(result==4)
+        result  =  linear_search(arr, search)
+        self.assertTrue(result==arr.index(search))
 if __name__=='__main__':
     unittest.main()
