cff8f40dbf198f41b1b303c2f1e6c9fa654e0c22
Luis Muñoz <lumusa2design@gmail.com>
2025-04-30 17:47:32 +0100

    continue with bfs

--- graph_traversal_algorithms/BFS.py
+++ graph_traversal_algorithms/BFS.py
@@ -1,4 +1,16 @@
 from non_directed_graph import build_graph
+from sys import maxsize
+from data_structures.Queue import Queue
+import networkx as nx
 
-def BFS(graph):
-    print("Por ahora esto")
+def BFS(graph, first_node):
+    visited, viewed, queue = [], [], Queue()
+    viewed.append(first_node)
+    queue.enqueue(first_node)
+    while not queue.is_empty():
+        actual_node = queue.pop()
+        if actual_node not in visited:
+            visited.append(actual_node)
+
+
+
