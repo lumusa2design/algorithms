cb77657f5de61971ce506f3669726623b0d5a48b
Luis Muñoz Sanz <lumusa2design@gmail.com>
2025-05-31 02:17:20 +0100

    binary conversion

---
+++ search algorithms/binary_conversion.py
@@ -0,0 +1,8 @@
+def dec_to_binary(number):
+    result = ''
+    while number != 0:
+        result = str(number %2) + result
+        number = number // 2
+    return result or '0'
+
+print(dec_to_binary(0))
