75e8ee765785e7914ce03ded5bf0c607b84276ff
Luis Muñoz Sanz <lumusa2design@gmail.com>
2025-06-11 00:15:27 +0100

    feat: count ways

---
+++ search_algorithms/count_ways.py
@@ -0,0 +1,6 @@
+def count_ways(number_of_weights):
+    if number_of_weights == 0 or number_of_weights==1:
+        return 1
+    return count_ways(number_of_weights-1) + count_ways(number_of_weights-2)
+
+print(count_ways(4))
