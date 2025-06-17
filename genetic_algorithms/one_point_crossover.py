795679c1586c763ed69130b1d496729bcc9f365a
Luis Muñoz Sanz <lumusa2design@gmail.com>
2025-06-17 17:32:17 +0100

    feat: lambda one point crossover

--- genetic_algorithms/one_point_crossover.py
+++ genetic_algorithms/one_point_crossover.py
@@ -1,3 +1,5 @@
 def one_point_crossover(point, father, mother):
     father[:point], mother[:point] = mother[:point], father[:point]
     return father, mother
+
+lambda_one_point_crossover = lambda point,father, mother : [mother[:point] ,father[:point]]
