7b74b5bcf779add83a33a5787b653b2483672306
Luis Muñoz Sanz <lumusa2design@gmail.com>
2025-06-18 15:23:05 +0100

    feat: lambda two point crossover

--- genetic_algorithms/two_point_crossover.py
+++ genetic_algorithms/two_point_crossover.py
@@ -1,3 +1,5 @@
 def two_point_crossover(first_point, second_point, father, mother):
     father[first_point:second_point],mother[first_point:second_point] = mother[first_point:second_point],father[first_point:second_point]
     return father, mother
+
+lambda_two_point_crossover = lambda first_point, second_point, father, mother: (mother[:first_point] + father[first_point:second_point] + mother[second_point:], father[:first_point] + mother[first_point:second_point] + father[second_point:])
