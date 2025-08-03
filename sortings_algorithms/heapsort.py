5f1ceb674c396523cf8fd8e1e844292ab345832e
Luis Muñoz Sanz <lumusa2design@gmail.com>
2025-08-03 13:59:11 +0100

    feat: heapsort

--- sortings_algorithms/heapsort.py
+++ sortings_algorithms/heapsort.py
@@ -1,11 +1,8 @@
 from data_structures.heap import Heap
 def heapsort(array):
     heap = Heap()
-
     for i in range(len(array)):
         heap.insert(array[i])
-
     for i in range(len(array)):
         array[i] = heap.extract()
-
     return array
