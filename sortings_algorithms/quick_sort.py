f58bedc6c5efe7c374f05c6696e1b8029310a41a
Luis Muñoz Sanz <lumusa2design@gmail.com>
2025-09-14 17:04:31 +0100

    refactor: quicksort

--- sortings_algorithms/quick_sort.py
+++ sortings_algorithms/quick_sort.py
@@ -1,15 +1,26 @@
-def quicksort(arr):
-    if not arr:
-        return []
-    if len(arr) == 1:
-        return arr
-    left = []
-    right=[]
-    pivot = arr[-1]
-    for i in arr[:-1]:
-        if i <= pivot:
-            left.append(i)
-        else:
-            right.append(i)
-    res = quicksort(left) + [pivot] + quicksort(right)
-    return res
+def quicksort(arr, low=0, high=None):
+    if high is None:
+        high= len(arr) -1
+
+    if low >= high:
+        return
+    pivot = partition(arr, low, high)
+
+    quicksort(arr, low, pivot)
+    quicksort(arr, pivot +1, high)
+
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+
+def partition(arr, low, high):
+    pivot, index_low, index_high = arr[(low + high)//2], low, high
+
+    while True:
+        while arr[index_low] < pivot:
+            index_low = index_low +1
+        while arr[index_high] > pivot :
+            index_high = index_high -1
+        if index_low >= index_high:
+            return index_high
+        swap(arr, index_low, index_high)
+
