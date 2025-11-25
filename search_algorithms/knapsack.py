104314314116f92521bad7bcee5309abb0933c49
Luis Muñoz Sanz <lumusa2design@gmail.com>
2025-11-25 21:47:51 +0000

    feat: knapsack problem

--- search_algorithms/knapsack.py
+++ search_algorithms/knapsack.py
@@ -1,2 +1,16 @@
 def knapsack(capacity, weights, values, repetition=True):
-    
+    size = len(weights)
+    best_value = [0] * (capacity + 1)
+
+    if repetition:
+        for i in range(capacity+1):
+            for j in range(size):
+                if weights[j] <= i:
+                    best_value[i] = max(best_value[i], best_value[i - weights[j]] + values[j])
+    else:
+        for j in range(size):
+            for i in range(capacity, weights[j]-1, -1):
+                best_value[i] = max(best_value[i], best_value[i - weights[j]] + values[j])
+
+    return best_value[capacity]
+
