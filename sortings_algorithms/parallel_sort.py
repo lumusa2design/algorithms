6abb98b6302df9cc4f70205f367d00d8c28ec170
lumusa2design <lumusa2design@gmail.com>
2026-04-08 15:23:11 +0100

    FEAT: Parallel sort

--- sortings_algorithms/parallel_sort.py
+++ sortings_algorithms/parallel_sort.py
@@ -12,14 +12,3 @@ def parallel_sort(arr, num_processes):
         sorted_chunks = list(executor.map(sorted, chunks))
 
     return list(heapq.merge(*sorted_chunks))
-
-if __name__ == "__main__":
-
-    import random
-
-    datos = [random.randint(0, 1000000)
-             for _ in range(100000)]
-
-    resultado = parallel_sort(datos, 2)
-
-    print(resultado[:10])
