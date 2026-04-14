2904ceeb16cff0e660d6ef2e0c4b2510a60e24a9
lumusa2design <lumusa2design@gmail.com>
2026-04-14 11:25:11 +0100

    FIX: dependency of timsort

--- sortings_algorithms/hybrid_adaptative_parallel_sort.py
+++ sortings_algorithms/hybrid_adaptative_parallel_sort.py
@@ -1,7 +1,7 @@
 from concurrent.futures import ProcessPoolExecutor
 from sortings_algorithms.insertion_sort import insertion_sort
 from auxiliar_functions import disorder_percent
-from sortings_algorithms.timsort import timsort
+from sortings_algorithms.tim_sort import tim_sort
 import heapq
 
 
@@ -11,7 +11,7 @@ def hybrid_adaptative_parallel_sort(arr, num_processes):
     if len(arr) < 1000: 
         return insertion_sort(arr)
     if disorder_percent(arr) < 0.1: 
-        return timsort(arr)
+        return tim_sort(arr)
     chunk_size = len(arr) // num_processes
     chunks = [arr[i:i + chunk_size] for i in range(0, len(arr), chunk_size)]
 
