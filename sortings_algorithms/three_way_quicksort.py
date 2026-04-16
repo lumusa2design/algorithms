dc73db20ca8b013d879d61deb0f1093beb2af111
lumusa2design <lumusa2design@gmail.com>
2026-04-16 12:11:21 +0100

    FEAT: Three way quicksort

---
+++ sortings_algorithms/three_way_quicksort.py
@@ -0,0 +1,24 @@
+def three_way_quicksort(arr, low, high):
+    if low < high:
+        lt, gt = partition(arr, low, high)
+        three_way_quicksort(arr, low, lt - 1)
+        three_way_quicksort(arr, gt + 1, high)
+
+def partition(arr, low, high):
+    pivot = arr[low]
+    lt = low
+    gt = high
+    i = low + 1
+
+    while i <= gt:
+        if arr[i] < pivot:
+            arr[lt], arr[i] = arr[i], arr[lt]
+            lt += 1
+            i += 1
+        elif arr[i] > pivot:
+            arr[i], arr[gt] = arr[gt], arr[i]
+            gt -= 1
+        else:
+            i += 1
+
+    return lt, gt
