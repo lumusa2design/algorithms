35d915497f3c64d32ed223aa90d826a3ffe48a42
lumusa2design <lumusa2design@gmail.com>
2026-05-07 16:21:12 +0100

    IMPROVE: a little improve in velocity of hybrid adaptative parallel sort

--- sortings_algorithms/hybrid_adaptative_parallel_sort.py
+++ sortings_algorithms/hybrid_adaptative_parallel_sort.py
@@ -7,30 +7,33 @@ from three_way_quicksort import three_way_quicksort
 from auxiliar_functions import can_use_counting_sort
 from counting_sort import counting_sort
 from parallel_sort import parallel_sort
+from auxiliar_functions import analyze_array
 import heapq
 
 
 def hybrid_adaptive_parallel_sort(arr, num_processes):
-    if len(arr) <= 1:
+    features = analyze_array(arr)
+
+    n = features["n"]
+
+    if n <= 1:
         return arr, "trivial"
 
-    n = len(arr)
-
-    if n < 64:
-        return insertion_sort(arr), "insertion_sort"
-
-    if duplicate_percent(arr) > 0.25 and can_use_counting_sort(arr):
+    if n < 64 and features["can_counting"]:
         return counting_sort(arr), "counting_sort"
 
-    if disorder_percent(arr) < 0.1:
+    if features["duplicates"] > 0.25 and features["can_counting"]:
+        return counting_sort(arr), "counting_sort"
+
+    if features["disorder"] < 0.1:
         tim_sort(arr)
         return arr, "tim_sort"
 
-    if duplicate_percent(arr) > 0.3:
+    if features["duplicates"] > 0.3:
         three_way_quicksort(arr, 0, n - 1)
         return arr, "three_way_quicksort"
 
-    if n > 100_000:
+    if n > 1_000_000:
         return parallel_sort(arr, num_processes), "parallel_sort"
 
     tim_sort(arr)
