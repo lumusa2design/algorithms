c45fa56ac6b60085f6d6b45a31f995343324de04
lumusa2design <lumusa2design@gmail.com>
2026-05-12 14:30:21 +0100

    FEAT: merge

--- sortings_algorithms/runawareblocksort.py
+++ sortings_algorithms/runawareblocksort.py
@@ -11,3 +11,21 @@ def detect_runs(arr):
             start = i
     runs.append(arr[start:n])
     return runs
+
+
+
+def merge(left, right):
+    merged = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+    return merged
+
+
