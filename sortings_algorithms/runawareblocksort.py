e7a53ff47030438f29e49212e88d798c874ba757
lumusa2design <lumusa2design@gmail.com>
2026-05-13 14:30:21 +0100

    FEAT: run aware merge sort

--- sortings_algorithms/runawareblocksort.py
+++ sortings_algorithms/runawareblocksort.py
@@ -29,3 +29,24 @@ def merge(left, right):
     return merged
 
 
+
+def run_aware_merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    runs = detect_runs(arr)
+
+    while len(runs) > 1:
+        new_runs = []
+
+        for i in range(0, len(runs), 2):
+            if i + 1 < len(runs):
+                new_runs.append(merge(runs[i], runs[i + 1]))
+            else:
+                new_runs.append(runs[i])
+
+        runs = new_runs
+
+    return runs[0]
+
+
