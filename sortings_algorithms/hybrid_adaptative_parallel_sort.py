2f3f76a497149f37e7f256d30647621ac7655f28
lumusa2design <lumusa2design@gmail.com>
2026-06-12 13:02:56 +0100

    FEAT: add to adaptative parallel sort the three way quicksort

--- sortings_algorithms/hybrid_adaptative_parallel_sort.py
+++ sortings_algorithms/hybrid_adaptative_parallel_sort.py
@@ -2,6 +2,8 @@ from concurrent.futures import ProcessPoolExecutor
 from sortings_algorithms.insertion_sort import insertion_sort
 from auxiliar_functions import disorder_percent
 from sortings_algorithms.tim_sort import tim_sort
+from sortings_algorithms.auxiliar_functions import duplicate_percent
+from sortings_algorithms.three_way_quicksort import three_way_quicksort
 import heapq
 
 
@@ -12,6 +14,8 @@ def hybrid_adaptative_parallel_sort(arr, num_processes):
         return insertion_sort(arr)
     if disorder_percent(arr) < 0.1: 
         return tim_sort(arr)
+    if duplicate_percent(arr) > 0.5: 
+        return three_way_quicksort(arr, 0, len(arr) - 1)
     chunk_size = len(arr) // num_processes
     chunks = [arr[i:i + chunk_size] for i in range(0, len(arr), chunk_size)]
 
